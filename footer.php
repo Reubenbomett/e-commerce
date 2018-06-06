7406fbc32f5c84cf9eb9c696950ffd155c6d769b
Reubenbomett <39495122+Reubenbomett@users.noreply.github.com>
2018-06-06 11:35:58 +0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,5 @@
+<!-- My footer  -->
+<div class="bg-darker icon" style="padding:10px; position:fixed; bottom:0px; width:100%">
+	<span class="subheader fg-lime offset3">&copy the KABUGUY</span>
+	<span class="fg-white icon-twitter offset1">&nbsp @ItsOncharrey</span><br>
+</div>
